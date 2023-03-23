df2eb37dbb749895176bf24472ebab69adc3367c
gloria-gee <glorygitonga14@gmail.com>
2023-03-23 12:22:25 -0700

    variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,29 +1,24 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 
-	/**
-	 * sum_them_all - Returns the sum of all its paramters.
-	 * @n: The number of paramters passed to the function.
-	 * @...: A variable number of paramters to calculate the sum of.
-	 *
-	 * Return: If n == 0 - 0.
-	 *         Otherwise - the sum of all parameters.
-	 */
-	int sum_them_all(const unsigned int n, ...)
-	{
-		va_list ap;
-		unsigned int i, sum = 0;
+/**
+ * sum_them_all - calculates the sum of all it's parameters
+ * @n: number of arguments passed to the function
+ *
+ * Return: the resulting sum
+ */
 
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list list;
 
-		va_start(ap, n);
+	va_start(list, n);
 
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
 
-		for (i = 0; i < n; i++)
-			sum += va_arg(ap, int);
+	va_end(list);
 
-
-		va_end(ap);
-
-
-		return (sum);
-	}
+	return (sum);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,12 @@
-#ifndef _VARIADIC_H_
-#define _VARIADIC_H_
-        
-	int _putchar(char c);
-        int sum_them_all(const unsigned int n, ...);
-        void print_numbers(const char *separator, const unsigned int n, ...);
-        void print_strings(const char *separator, const unsigned int n, ...);
-        void print_all(const char * const format, ...);
-        #endif
+#ifndef VARIADIC_FUNC_H
+#define VARIADIC_FUNC_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNC */
