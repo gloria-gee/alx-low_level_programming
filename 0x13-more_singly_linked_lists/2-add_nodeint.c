649729f50860e25ad5cfbead4f47333637408620
Glory Gitonga <103115158+gloria-gee@users.noreply.github.com>
2023-04-05 21:34:42 +0300

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,7 +2,8 @@
 
 /**
  * add_nodeAdd a new node to the beginning of a linked list with the following syntax: int 
- * @head: pointer to the list's initial node * @n: data to insert in the new node 
+ * @head: pointer to the list's initial node 
+ * @n: data to insert in the new node 
  * Upon successful completion, return a reference to the new node or NULL.
  */
 
