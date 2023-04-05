c55dd64e5e36a7f001ce988ed5ec1523ade34bf5
Glory Gitonga <103115158+gloria-gee@users.noreply.github.com>
2023-04-05 21:31:56 +0300

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * Frees a linked list using the function free_listint 
- * @head: listint_t _t list to be freed
+ * Utilizing the function free_listint, a linked list is released. 
+ * @head: freeing the listint_t _t list
  */
 
 void free_listint(listint_t *head)
